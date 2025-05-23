8da059ea026f7c45793b67fa9f2758aee0643ef7
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2025-05-23 15:01:57 -0400

    run prettier for format

--- src/pages/csc368/pipelining/pipelining.tsx
+++ src/pages/csc368/pipelining/pipelining.tsx
@@ -4,16 +4,16 @@ import { BasicLaundry } from "./components";
 
 export const Pipelining: React.FunctionComponent = () => {
   return (
-    <div className="flex flex-col items-center p-8 gap-6 min-h-screen">
-      <h1 className="scroll-m-20 text-3xl font-bold tracking-tight mb-2">
+    <div className="flex min-h-screen flex-col items-center gap-6 p-8">
+      <h1 className="mb-2 scroll-m-20 text-3xl font-bold tracking-tight">
         Instruction Pipelining Basics: The Laundry Analogy
       </h1>
-      
-      <div className="w-full max-w-4xl bg-white p-6 rounded-lg shadow-md">
+
+      <div className="w-full max-w-4xl rounded-lg bg-white p-6 shadow-md">
         <BasicLaundry />
       </div>
-      
-      <div className="flex gap-[16px] mt-4">
+
+      <div className="mt-4 flex gap-[16px]">
         <Button asChild variant="outline">
           <a href="/csc368">Back to CSC368</a>
         </Button>

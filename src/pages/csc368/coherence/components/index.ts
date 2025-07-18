3c0fd2a6b20b9aaf007ade428c82fd81a15bf36e
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2025-07-18 01:04:22 -0400

    clean up

--- src/pages/csc368/coherence/components/index.ts
+++ src/pages/csc368/coherence/components/index.ts
@@ -1,4 +1,1 @@
-export { BinaryBlock } from "./BinaryBlock";
-export { TrueFalseSharingViz } from "./Coherence";
-export { FalseSharingGrid } from "./FalseSharingGrid";
 export { SimpleFalseSharingViz } from "./SimpleFalseSharingViz";
